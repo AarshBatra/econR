97d305813cec02da928f992fc7f599e33fc69a82
AarshBatra <aarshbatra.in@gmail.com>
2021-11-13 18:11:56 +0530

    count_missing_val function renamed to count_missing_values and now also includes relevant sanity checks

--- R/dailyLogicChecks.R
+++ R/dailyLogicChecks.R
@@ -8,28 +8,76 @@
 #' variables in a tidy format.
 #'
 #' @importFrom purrr map
-#' @importFrom dplyr rename
+#' @importFrom dplyr rename as_tibble
+#' @importFrom tibble is_tibble
 #' @importFrom broom tidy
+#'
+#' @param data dataset(\code{tibble}/\code{data.frame}) object from which the
+#' variable names are chosen.
 #'
 #' @param variable_names_vec a vector of variable names. For each of these
 #'                           variables, the function calculates the number
 #'                           of missing values present in their list of
-#'                           values.
+#'                           values. If you want to see missing values for
+#'                           all variables (columns) in the dataset, set
+#'                           \code{variable_names_vec = "all"}.
 #'
-#' @param data underlying dataset from which the variable names are
-#'             picked.
+#'
 #'
 #' @examples
-#' count_missing_val(variable_names_vec = c("a", "b"), data = data)
+#' count_missing_val(data = data, variable_names_vec = c("a", "b"))
+#' count_missing_val(data = data, variable_names_vec = "all")
+#' count_missing_val(data = data) # uses default \code{variable_names_vec = "all"}
 #'
 #' @return
 #'
 #' @export
 
-count_missing_val <- function(variable_names_vec, data){
-  dummy_data[, variable_names_vec] %>%
-    purrr::map(function(x) sum(is.na(x))) %>%
-    unlist() %>%
-    broom::tidy() %>%
-    dplyr::rename("variable_name" = "names", "num_missing_values" = "x")
+
+count_missing_values <- function(df, variable_names_vec = "all"){
+
+  # "df" object should exist and should be a tibble/dataframe
+  if((envnames::get_obj_name(df) %in% ls()) &&
+     (tibble::is_tibble(df) || is.data.frame(df))){
+
+    df <- dplyr::as_tibble(df)
+
+    # view missing values for all variables (columns) in the df
+    if(variable_names_vec == "all"){
+
+      return(df %>%
+        purrr::map(function(x) sum(is.na(x))) %>%
+        unlist() %>%
+        broom::tidy() %>%
+        dplyr::rename("variable_name" = "names", "num_missing_values" = "x"))
+
+    # view missing values only for selected variables in the "variable_names_vec"
+    # argument
+    } else if (sum(variable_names_vec %in% colnames(df)) ==
+               length(variable_names_vec)) {
+
+      return(df[, variable_names_vec] %>%
+        purrr::map(function(x) sum(is.na(x))) %>%
+        unlist() %>%
+        broom::tidy() %>%
+        dplyr::rename("variable_name" = "names", "num_missing_values" = "x"))
+
+    # check if atleast one of the variable (column) names entered in the "variable_names_vec"
+    # argument does not exist in the underlying dataset entered in "df".
+    } else {
+      stop("All the variable (columns) names present in the vector 'varibale_names_vec' should
+         exist in the underlying dataset entered into 'data'. Please make sure that
+         this is the case, before proceeding.")
+    }
+
+  } else {
+    stop("Either the object doesn't exist or it isn't a tibble/dataframe. Please make
+         sure that the object exists in the current environment and also make
+         sure that it is of class: 'data.frame'/'tbl'/'tbl_df'. To coerce an object to
+         to tibble you can use: 'as_tibble' function.")
+  }
+
 }
+
+
+
